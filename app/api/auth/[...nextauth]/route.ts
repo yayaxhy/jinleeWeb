edc2489f5064061a7ee9a97f162abdf912412c77
James <jamesfang1116@gmail.com>
2025-11-16 19:36:16 -0500

    Add authorization params for Discord provider

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -8,6 +8,11 @@ export const authOptions = {
     DiscordProvider({
       clientId: process.env.DISCORD_CLIENT_ID ?? '',
       clientSecret: process.env.DISCORD_CLIENT_SECRET ?? '',
+      authorization: {
+        params: {
+          scope: 'identify guilds.members.read',
+        },
+      },
     }),
   ],
   callbacks: {
