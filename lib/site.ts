15c5141dfb1d8a287c41c91c8d43799074199a34
YaYa <jamesfang1116@gmail.com>
2026-07-09 20:21:43 +0200

    Update site.ts

--- lib/site.ts
+++ lib/site.ts
@@ -2,4 +2,4 @@ export const SITE_URL = 'https://jinleeclub.vip';
 export const SITE_NAME = '锦鲤公会';
 export const SITE_ALTERNATE_NAME = 'Jinlee Club';
 export const SITE_DESCRIPTION =
-  '锦鲤公会（Jinlee Club）游戏陪玩社区，提供 Valorant、Overwatch 等游戏的陪玩信息与公会服务。';
+  'Jinlee锦鲤欧服陪玩公会，游戏陪玩社区，提供瓦，英雄联盟，三角洲，OW等游戏的陪玩服务。';
